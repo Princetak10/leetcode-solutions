ba6937ccae4edd5c8e8eb6121a4135b17f9b7dd1
Prince Tak <144263995+Princetak10@users.noreply.github.com>
2026-09-21 01:59:26 +0530

    Time: 0 ms (100%), Space: 9.7 MB (78.56%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+         int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int reversePos = 26 - (s[i] - 'a');
+            int index = i + 1;
+            ans += reversePos * index;
+        }
+        return ans;
+    }
+};
